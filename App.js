09dfcfeb614cec291208b605f849346089e287c3
Rick6dev <ricardoweb6dev@gmail.com>
2024-09-29 22:21:07 -0400

    feautere:Inicio del proyecto

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+  Pressable,
+} from "react-native";
 
+const icon = require("./assets/icon.png");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: "https://picfiles.alphacoders.com/432/thumb-432394.jpg",
+        }}
+        style={{ width: 200, height: 300 }}
+      />
+      <Text>Hola Puticosas!</Text>
+      <Button
+        title="Pulsa Aqui"
+        onPress={() => alert("Hola Perras")}
+        color="green"
+      ></Button>
+
+      <TouchableHighlight
+        underlayColor={"red"}
+        onPress={() => alert("Hola Perras")}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "blue",
+          borderRadius: 200,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Pulsa Aqui</Text>
+      </TouchableHighlight>
+
+      <Pressable
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? "red" : "green" },
+        ]}
+      >
+        {({ pressed }) => <Text>{pressed ? "Presionado" : "Presiona"}</Text>}
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
